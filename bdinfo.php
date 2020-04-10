6730c4109a0b636a9f76f538f5e5a7dad5921f8d
LC7627CR <49035449+LC7627CR@users.noreply.github.com>
2020-04-09 20:39:33 -0600

    2
    2

--- bdinfo.php
+++ bdinfo.php
@@ -1,17 +1,68 @@
 <?php
-$link = mysql_connect("localhost","root", "") or die ("<h2> No se encuentra el servidor </h2>")
-$db = mysql_select_db(bdinfo,$link) or die ("<h2> Error de conexion </h2>")
+$servidor ="localhost";
+$usuario ="root";
+$contraseña="";
+$baseDeDatos ="bdinfo";
 
-$agente = $_Post['agentebdinfo'];
-$codigo =$_Post['codigobdinfo'];
-$nombre = $_Post['nombrebdinfo'];
-$apellido = $_Post['apellidobdinfo'];
+$enlace =mysqli_pconnect($servidor, $usuario, $contraseña, $baseDeDatos);
+if(!$enlace){
 
-$req = (strlen ($agente) *strlen($codigo) *strlen($nombre) *strlen($apellido) ) or die ("No se han completado todos los datos")
+	echo "Error en  la conexion con el servidor";
+}
 
-mysql_query("INSERT INTO bdinfo VALUES ($agente', '$codigo', '$nombre', '$apellido')", $link ) or die(<h2> error en enviar</h2>)
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Tu info</title>
+</head>
+<body bgcolor="gray">
+	
+	<center><h1>Actualiza tus datos</h1></center>
+<img src="imagenes\monge.jpg" height="20%" width="20%" >
+	<form action="bdinfo.php" method="POST">
 
-echo '<h1>Gracias</h1>
-' ;
+<div id="registro">
+	<table>
+		<tr>
+		<td><label for="agente">Agente:</label>
+		<td><input type="text" name="agentebdinfo" id="agente" placeholder="agente"></td>
+		</tr>
+		<tr>
+		<td><label for="codigo"> codigo: </label></td>
+		<td><input type="text" name="codigobdinfo" id="codigo" 	placeholder="codigo"></table></td>
+		</tr>
+		<tr>
+		<td><label for="nombre"> Nombres:</label></td>
+		<td><input type="text" name="nombrebdinfo" id="nombre" placeholder="nombres"></td>
+		</tr>
+		<tr>
+		<td><label for="apellido" >Apellidos:</label></td>
+		<td><input type="text" name="apellidobdinfo" id="apellido" placeholder="apellidos"></td>
+		</tr>	
+		<p><input type="submit" name="guardar" 	value="Guardar">
+		<input type="reset" value="Modificar"></p>
+	</table>
+	</form>
+	</div>
+</body>
+</html>
 
-?>	
+<?php 
+if (isset($_POST['guardar'])) {
+
+$insertarDatos ="INSERT INTO bdinfo VALUES ($agente', 
+											'$codigo', 
+											'$nombre', 
+											'$apellido')":
+$ejecutarinsertar=mysqli_query($enlace, $insertarDatos);
+
+if ($ejecutarinsertar) {
+	echo "error en la conexion";
+}
+
+}
+
+
+?>
